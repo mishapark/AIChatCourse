0968d2530add4c18c2638ec9fd4ac7b876934482
mishapark <packmisha@mail.ru>
2026-01-06 21:39:30 -0500

    Initial Commit

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by Mikhail Pak on 2026-01-06.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by Mikhail Pak on 2026-01-06.
+//
+
+import Testing
+@testable import AIChatCourse
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
